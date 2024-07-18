d1e39a2a7906454af72737ede2be8879ac2617ef
Valbertth Alves <109263991+Valbertth08@users.noreply.github.com>
2024-07-18 10:07:18 -0300

    adicionado o Docheck

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,12 +6,14 @@ import { Component, DoCheck, OnDestroy } from '@angular/core';
   styleUrl: './home.component.css'
 })
 //essa função é chamada quando um componente é parado de ser exebido na tela, ou seja ele foi destruido,  e é chamada de forma automaica essa função.
-export class HomeComponent implements OnDestroy, DoCheck {
-  ngOnDestroy(): void {
-    console.log("destruiu")
-  }
+export class HomeComponent implements  DoCheck {
+
+  num: number=1;
   //responsavel por verifica se teve alguma alteração no componente
   ngDoCheck(): void {
     console.log("uma mudança foi feita")
+  }
+  adiciona1(){
+    this.num++;
   }
 }
