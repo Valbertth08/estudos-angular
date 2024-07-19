4a266fc63d8a49fad34a63fe04c48063f552f0c8
Valbertth Alves <109263991+Valbertth08@users.noreply.github.com>
2024-07-19 09:11:50 -0300

    aplicando rotas de paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './_components/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //path: "caminho", component: componente para o caminho
+  {path: '',component: HomeComponent},//indico que esse componente tem que ser carregado na raiz do programa
+  {path: 'users',component: UsersComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
